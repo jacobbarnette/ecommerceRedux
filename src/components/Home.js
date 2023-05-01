a282c9f7acfa9fe6d8b49acc45610ab27c1f256e
jacobbarnette <jacobcbarnette@gmail.com>
2023-05-01 15:56:56 -0400

    updated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { Badge } from "react-bootstrap";
+import { Badge, Image } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import homepageImg from "../Assets/homepage-image.png";
 import { motion } from "framer-motion";
@@ -14,22 +14,22 @@ const Home = () => {
     >
       <div className="container">
         <div className="home row">
-          <div className="col xl-6 col-lg-6  col-md-12 ">
-            <h4 className="tagLine tagLineTop mt-5 text-center text-muted">
+          <div className="col xl-6  taglineContainer col-lg-6 px-5 col-md-12 ">
+            <h4 className="tagLine tagLineTop   text-center text-muted">
               Voted Best Retail Site of 2022
             </h4>
             <p className="tagLine">We don't follow trends, we set them.</p>
             <Badge
               onClick={() => navigate("/items")}
               onTouchStart={() => navigate("/items")}
-              className="rounded-pill shopBtn"
+              className="rounded-pill mx-auto shopBtn"
               bg="dark"
             >
               Shop Now
             </Badge>
           </div>
-          <div className="col-lg-6 mt-5 col-md-6 col-sm-6">
-            <img className="homeImg" src={homepageImg} alt="people"></img>
+          <div className="col-lg-6   col-md-6 col-sm-6">
+            <Image className="homeImg" src={homepageImg} alt="people"></Image>
           </div>
         </div>
       </div>
